3b1d5e6c4551d912b0aae1426e3ba506d97f23e3
Hal Seki <hal@Hals-MacBook-Pro.local>
2021-04-30 01:09:04 +0900

    removed console.log

--- docs/module.js
+++ docs/module.js
@@ -42,7 +42,6 @@ const ROTATE = true;
 const createChapters = (chapters) => {
   let n = 0;
   return chapters.map((chapter) => {
-    console.log(chapter)
     n += 1;
     chapter.id = `chapter-${n}`;
     chapter.alignment = ALIGNMENT;
@@ -67,19 +66,13 @@ const createChapters = (chapters) => {
 }
 
 const process = (config, callback) => {
-  console.log('process is called')
-  console.log(config)
   // config.accessToken = TOKEN;
   config.theme = 'light';
   config.showMarkers = false;
   if (typeof config.chapters === 'string') {
     if (config.chapters.indexOf('https://docs.google.com/spreadsheets/') === 0) {
       readGoogleSpreadsheet(config.chapters, (chapters) => {
-        console.log(chapters)
-        console.log('hhhhhhhh')
-        console.log(config)
         config.chapters = createChapters(chapters);
-        console.log(config)
         callback(config)
       })
     }
@@ -103,15 +96,11 @@ if (!yml) {
   alert("error: not found YAML");
 } else {
   process(YAML.parse(yml), (config) => {
-    console.log('hoge')
-    console.log(config)
     window.config = config
     if (typeof (mapboxgl) !== 'undefined') {
-      console.log('showMap1')
       showMap(config);
     } else {
       window.onload = function () {
-        console.log('showMap2')
         showMap(config)
       }
     }
